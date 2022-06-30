e768184ede2db8736c539fae78fbc05494af14c0
Peter <peter.kostoglod@gmail.com>
2022-06-30 11:50:19 +0300

    Warning + working form

--- src/components/contactComponent/ContactForm.jsx
+++ src/components/contactComponent/ContactForm.jsx
@@ -1,15 +1,31 @@
-import React, {useState} from 'react';
+import React, {useRef, useState} from 'react';
 import './ContactForm.template.css'
 import phone from "../../icons/phone.png";
 import email_ico from "../../icons/email.png";
 import address from "../../icons/address.png";
 import {useTranslation} from "react-i18next";
+import emailjs from '@emailjs/browser';
 
 const ContactForm = () => {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [message, setMessage] = useState('');
+    const form = useRef();
     const {t} = useTranslation()
+
+    const sendMessage = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_b9bfh8a', 'template_8rju3mf', form.current, 'V0Einrog7BysY6DGQ')
+            .then((result) => {
+                alert("Message Sent Successfully");
+                setName("")
+                setEmail("")
+                setMessage("")
+            }, (error) => {
+                console.log(error.text)
+            })
+    }
 
     return (
         <div id="contacts">
@@ -33,43 +49,46 @@ const ContactForm = () => {
                     </div>
                 </div>
                 <div className="right-address">
-                    <div className="form-group">
-                        <label htmlFor="name">
-                            {t('form-name')}
-                            <input
-                                type="text"
-                                id="name"
-                                name="name"
-                                value={name}
-                                onChange={(e) => setName(e.target.value)}
-                            />
-                        </label>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="email">
-                            {t('form-email')}
-                            <input
-                                type="email"
-                                id="email"
-                                name="email"
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}
-                            />
-                        </label>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="message">
-                            {t('form-message')}
-                            <textarea
-                                type="text"
-                                id="message"
-                                name="message"
-                                value={message}
-                                onChange={(e) => setMessage(e.target.value)}
-                            />
-                        </label>
-                    </div>
-                    <button type="submit">{t('form-button')}</button>
+                    <form onSubmit={sendMessage} ref={form}>
+                        <div className="form-group">
+                            <label htmlFor="name">
+                                {t('form-name')}
+                                <input
+                                    type="text"
+                                    id="name"
+                                    name="name"
+                                    value={name}
+                                    onChange={(e) => setName(e.target.value)}
+                                />
+                            </label>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="email">
+                                {t('form-email')}
+                                <input
+                                    type="email"
+                                    id="email"
+                                    name="email"
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                />
+                            </label>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="message">
+                                {t('form-message')}
+                                <textarea
+                                    type="text"
+                                    id="message"
+                                    name="message"
+                                    value={message}
+                                    onChange={(e) => {e.preventDefault()
+                                        setMessage(e.target.value)}}
+                                />
+                            </label>
+                        </div>
+                        <button type="submit">{t('form-button')}</button>
+                    </form>
                 </div>
             </div>
         </div>
